2dc8bf999f2ecd8a9a8d20bfd4ad8c43159cb001
Levy Nunes <levy.vix@gmail.com>
2023-04-08 22:56:06 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fix episode names and remove all other files
+
+## How it works
+
+Take all the .mkv files and search for a regex pattern and them rename the entire file for that pattern, like S03E08.
+
---
+++ fix_names.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+import re
+
+EXTENSION = "mkv"
+
+# take all files in the current directory that ends with .mkv
+all_files = glob("*")
+files = glob(f"*.{EXTENSION}")
+subs = glob("*.srt")
+
+# loop through the files and rename them
+
+for file in files:
+    if "p" not in file:
+        continue
+
+    episode = re.search(r"\.(S\d+E\d+)", file).group(1)
+
+    # rename the entire file with only the episode name
+    new_name = f"{episode}.{EXTENSION}"
+
+    # rename the file
+    os.rename(file, new_name)
+
+
+for sub in subs:
+    os.remove(sub)
+
+# remove every COMANDO file
+for file in all_files:
+    if "COMANDO" in file:
+        os.remove(file)
